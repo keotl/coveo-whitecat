3fc4fd93a971ef013e0ac40b2f2eeb7e23991205
Sébastien Dufour-Beauséjour <sebastien.dufourbeausejour@gmail.com>
2018-01-06 10:51:07 -0500

    Fix build

--- runGame.bat
+++ runGame.bat
@@ -1,3 +1,1 @@
-javac ca.ligature.whitecat22.MyBot.java
-javac ca.ligature.whitecat22.RandomBot.java
-.\blitz2k18.exe -f 2 -d "30 30" "java ca.ligature.whitecat22.MyBot" "java ca.ligature.whitecat22.RandomBot"
+blitz2k18.exe -f 2 -d "30 30" "java -jar build\libs\coveo-whitecat.jar" "java -jar MyBot.jar"
